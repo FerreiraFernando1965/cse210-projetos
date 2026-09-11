f330558d69fff9eb657587f9cea0fcdc3e3e4318
FerreiraFernando1965 <fferreira2@byupathway.edu>
2026-09-11 17:03:00 -0300

    código Diario.cs

---
+++ semana01/Diario.cs
@@ -0,0 +1,33 @@
+public class Diario
+{
+    public List<Registro>_registros = new List<Registro>();
+
+    public void adicionarRegistro(Registro novoRegistro)
+
+    {
+        
+    }
+
+
+    public void ExibirTodos()
+
+    {
+        
+    }
+        
+    public void SalvarNoArquivo(string arquivo)
+
+    {
+        
+    }
+
+    
+    public void CarregarDoArquivo(string arquivo)
+
+    {
+    
+    }
+  
+ 
+}    
+    
---
+++ semana01/Registro.cs
@@ -0,0 +1,12 @@
+public class Registro
+{
+    public string _data;
+    public string _pergunta;
+    public string _resposta;
+
+    public void exibir()
+
+    {
+        
+    }
+}
